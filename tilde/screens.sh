ee38ec1f99c702b349678c413db81c8f032e73a6
Jace Denny <j.denny@ascentvision.com.au>
2025-10-29 21:22:16 +1100

    fix screens init

--- tilde/screens.sh
+++ tilde/screens.sh
@@ -4,17 +4,17 @@ if [[ $XDG_SESSION_TYPE == "wayland" ]]; then
     SCREEN=`hyprctl monitors all | grep Monitor | awk '{print $2}' | grep -v eDP-1`
 
     case $1 in
-        "1") hyprctl keyword monitor eDP-1,preferred,auto,auto &&
+        "1") hyprctl keyword monitor eDP-1,preferred,auto,1 &&
             hyprctl keyword monitor $SCREEN,disable;;
-        "b") hyprctl keyword monitor $SCREEN,preferred,auto,auto &&
+        "b") hyprctl keyword monitor $SCREEN,preferred,auto,1 &&
             hyprctl keyword monitor eDP-1,disable;;
-        "s") hyprctl keyword monitor eDP-1,preferred,auto,auto &&
+        "s") hyprctl keyword monitor eDP-1,preferred,auto,1 &&
             hyprctl keyword monitor $SCREEN,preffered,auto,auto,mirror,eDP-1;;
         *)
             if [[ -n $SCREEN ]]; then
                 ~/screens.sh b
             else
-                hyprctl keyword monitor ,preferred,auto,auto
+                hyprctl keyword monitor eDP-1,preferred,auto,1
             fi;;
         default);;
     esac
